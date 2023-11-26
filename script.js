bd97a2166a71d7d8fa41097b0b036e8fa0b21965
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-27 04:10:51 +0600

    add javascript eventlisteners for buttons

---
+++ script.js
@@ -0,0 +1,22 @@
+const facebookBtn = document.querySelector(".js-facebook-btn");
+const newQuoteBtn = document.querySelector(".js-new-quote-btn");
+
+const displayQuote = document.querySelector(".js-display-quote");
+const quoteAuthor = document.querySelector(".js-quote-author");
+
+facebookBtn.addEventListener("click", () => {
+  window.open(`http://facebook.com`, "_blank");
+})
+
+newQuoteBtn.addEventListener("click", () => {
+  newQuote("https://api.quotable.io/random");
+})
+
+async function newQuote(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  displayQuote.innerHTML = data.content;
+  quoteAuthor.innerHTML = data.author;
+}
+
+newQuote("https://api.quotable.io/random") //calling the function so that after loading the page it will show a quote from the api
